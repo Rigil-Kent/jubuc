90ca4e1a7973c4f57f4a21ea8856e537dad737fc
Bryan Bailey <31677949+Rigil-Kent@users.noreply.github.com>
2019-05-03 03:20:30 -0400

    second jubuc layout

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,19 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/index')
+def alt_index():
+    return render_template('layout2/index.html')
+
 @app.route('/photos')
 def photos():
     return render_template('photos.html')
+
+
+
+#region ADMINISTRATION
+@app.route('/admin')
+def admin_dashboard():
+    return render_template('layout2/admin/index.html')
+
+#endregion
